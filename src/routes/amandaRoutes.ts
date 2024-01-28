f0ec7a7d45665fd9707525a8703d19773929d40d
Paulo Henrique <phortolani@tmbeducacao.com.br>
2024-01-28 03:00:51 -0300

    ajustes na api

--- src/routes/amandaRoutes.ts
+++ src/routes/amandaRoutes.ts
@@ -6,8 +6,9 @@ const path = '/amanda'
 
 export default function AmandaRoutes(server: FastifyInstance, amandaService: AmandaService) {
     server.post(path, async (request, reply) => {
-        const amanda = request.body as Amanda
-        const amanda_saved = await amandaService.create(amanda)
+        const amanda = request.body as string
+        const obj = JSON.parse(amanda)
+        const amanda_saved = await amandaService.create(obj)
         if (amanda_saved) return reply.status(201).send(amanda_saved)
         return reply.status(502).send(`Não foi possível salvar o IP`)
     })
