46a92e419d50196ac6c876f702811d79b6ff2603
Paulo Henrique <phortolani@tmbeducacao.com.br>
2024-12-25 21:41:12 -0300

    new endpoint whatsapp

--- src/routes/whatsappRoutes.ts
+++ src/routes/whatsappRoutes.ts
@@ -25,4 +25,25 @@ export default function WhatsappRoutes(server: FastifyInstance, whatsappService:
             return reply.status(200).send(params['hub.challenge'])
         } else return reply.status(400)
     })
+
+
+    server.get(`${path}/webhook`, async (request, reply) => {
+        const { WA_CHALLENGE_TOKEN } = process.env
+
+        const params = request.query as ChallengeToken;
+        if (WA_CHALLENGE_TOKEN != params['hub.verify_token']) return reply.status(400).send('Invalid Token')
+
+        if (params['hub.challenge']) {
+            await whatsappService.create({
+                hub_mode: params['hub.mode'],
+                hub_challenge: params['hub.challenge'],
+                hub_verify_token: params['hub.verify_token'],
+                create_at: moment()
+            })
+            return reply.status(200).send(params['hub.challenge'])
+        } else return reply.status(400)
+    })
+
+
+
 }
